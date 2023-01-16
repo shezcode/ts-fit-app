b172f6f7c42a60105f8d5c5bfd15b05b8541a7ea
SHEZ <shezcode@gmail.com>
2023-01-16 11:40:51 +0100

    nav created, types and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/scenes/navbar"
+import { useState } from "react"
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home')
 
   return (
-    <div className="app">
-      app
+    <div className="app bg-gray-20 p-2">
+      <Navbar selectedPage={selectedPage}/>
     </div>
   )
 }
